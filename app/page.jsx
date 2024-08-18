a2c7173f9542e4968a468a7806058499d1f6dc77
resetmerlin <resetmerlin@gmail.com>
2024-08-18 15:17:16 +0900

    feat: Add Home Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,16 @@
-import { getAll } from '../data/db.js';
-import Like from './Like.jsx';
-import Cart from '../components/Cart/index.jsx';
+import { Suspense } from 'react';
+import Home from '../components/Home/index.jsx';
 import '../components/shared/global.scss';
+import { getAllProducts } from '../data/db.js';
 
-async function Albums() {
-	const albums = await getAll();
-	return (
-		<ul>
-			{albums.map((a) => (
-				<li key={a.id} className="flex gap-2 items-center mb-2">
-					<img className="w-20 aspect-square" src={a.cover} alt={a.title} />
-					<div>
-						<h3 className="text-xl">{a.title}</h3>
-						<p>{a.songs.length} songs</p>
-						<Like />
-					</div>
-				</li>
-			))}
-		</ul>
-	);
+export async function HomePage() {
+	const products = await getAllProducts();
+	return <Home products={products}></Home>;
 }
-
 export default async function Page() {
-	return <Cart></Cart>;
+	return (
+		<Suspense fallback="data is loading...">
+			<HomePage />
+		</Suspense>
+	);
 }
